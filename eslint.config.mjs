b5a06b9c9f66652d348366fbe8db4d2b7428bed8
tetsuo <ogunduz@gmail.com>
2025-03-24 11:46:35 +0100

    Add project files

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from '@eslint/js'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+  js.configs.recommended, // base JS rules
+
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module'
+    },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      curly: ['error', 'all'],
+      'no-constant-condition': 'warn',
+      'arrow-parens': ['error', 'as-needed'],
+      'no-console': 'off',
+      'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+      'no-debugger': 'warn',
+      'no-const-assign': 'error',
+      'no-var': 'error',
+      'prefer-arrow-callback': 'error',
+      'no-class-assign': 'error',
+      'no-dupe-class-members': 'off',
+      'space-before-function-paren': 'off',
+      'variable-name': 'off',
+      'ter-indent': 'off',
+      deprecation: 'off'
+    }
+  }
+]
